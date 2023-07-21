2b5aacc0b6e5b748df9e3cb07a1dd4cf24a9a64b
syedabdulazeez <syed.abdulazeez@kreeti.com>
2023-07-21 13:00:37 +0530

    safe login added

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -7,7 +7,7 @@ module Admin
     before_action :require_admin
 
     def require_admin
-      return if current_user.admin?
+      return if current_user&.admin?
 
       redirect_to root_path, alert: 'You are not authorized to access this page.'
     end
