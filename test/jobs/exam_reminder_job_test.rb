f0db1795d1b0fa7b5ed250b57a19d48793eb456b
syedabdulazeez <syed.abdulazeez@kreeti.com>
2023-08-02 14:05:44 +0530

    unnesessary files

--- test/jobs/exam_reminder_job_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class ExamReminderJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/registration_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class RegistrationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
